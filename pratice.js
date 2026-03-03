7f6db84f63c9f5a627889ecc0a5e9912ac7d4aa4
Your Name <your.email@example.com>
2026-03-03 12:56:03 +0530

    first commit

---
+++ pratice.js
@@ -0,0 +1,8 @@
+let imcrement = document.getElementById("a")
+let text = document.getElementById("b")
+
+function imcrementbtn(){
+    increment=0;
+    increment=increment+1;
+    text.textContent= increment
+}
