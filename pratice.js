f768de0f9a4d7a90f45226bb8ec703110b3de946
saidulupulipati <saidulupulipati092@gmail.com>
2026-03-03 13:03:23 +0530

    Delete pratice.js

--- pratice.js
+++
@@ -1,8 +0,0 @@
-let imcrement = document.getElementById("a")
-let text = document.getElementById("b")
-
-function imcrementbtn(){
-    increment=0;
-    increment=increment+1;
-    text.textContent= increment
-}
